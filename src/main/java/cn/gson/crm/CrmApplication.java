a078db789540c9899762fbeccca596cf6a2921ca
mht <952326644@qq.com>
2018-12-03 22:40:19 +0800

    添加启动日志

--- src/main/java/cn/gson/crm/CrmApplication.java
+++ src/main/java/cn/gson/crm/CrmApplication.java
@@ -1,5 +1,7 @@
 package cn.gson.crm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -7,8 +9,11 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class CrmApplication {
+    
+    private static final Logger logger = LoggerFactory.getLogger(CrmApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
+		logger.info("-------------------------启动完毕-------------------------");
 	}
 }
